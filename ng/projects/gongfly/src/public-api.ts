20e2729f1788a170f8923a431c4545d6b8e21d33
peugeot <thomaspeugeot@yahoo.fr>
2021-07-20 08:36:31 +0200

    new gongc

--- ng/projects/gongfly/src/public-api.ts
+++ ng/projects/gongfly/src/public-api.ts
@@ -79,7 +79,6 @@ export * from './lib/scenario.service'
 
 
 export * from './lib/ConceptEnum'
-export * from './lib/LayerEnum'
 export * from './lib/LinerStateEnum'
 export * from './lib/MessageStateEnum'
 export * from './lib/OperationalLineStateEnum'
