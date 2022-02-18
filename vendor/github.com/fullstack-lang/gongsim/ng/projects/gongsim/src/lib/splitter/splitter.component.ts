69ae0c87d8599ac8faa06f729842a8f62583b000
peugeot <thomaspeugeot@yahoo.fr>
2022-02-18 02:55:32 +0100

    meta (gong splitter) component does not display Fixes #5 - app.module.ts !!!!

--- vendor/github.com/fullstack-lang/gongsim/ng/projects/gongsim/src/lib/splitter/splitter.component.ts
+++ vendor/github.com/fullstack-lang/gongsim/ng/projects/gongsim/src/lib/splitter/splitter.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplitterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("init")
+  }
 
   ngOnInit(): void {
+    console.log("init")
   }
 }
