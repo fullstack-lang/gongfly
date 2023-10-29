95046e50b68f860b087cd9a1bd3b02b61e2d35de
peugeot <thomaspeugeot@yahoo.fr>
2023-10-29 19:40:45 +0100

    front ignore

--- ng/projects/gongfly/src/public-api.ts
+++ ng/projects/gongfly/src/public-api.ts
@@ -11,27 +11,6 @@ export * from './lib/commitnbfromback.service'
 export * from './lib/push_from_front_nb.service'
 
 
-export * from './lib/civilianairport-db'
-export * from './lib/civilianairport.service'
-
-export * from './lib/liner-db'
-export * from './lib/liner.service'
-
-export * from './lib/message-db'
-export * from './lib/message.service'
-
-export * from './lib/opsline-db'
-export * from './lib/opsline.service'
-
-export * from './lib/radar-db'
-export * from './lib/radar.service'
-
-export * from './lib/satellite-db'
-export * from './lib/satellite.service'
-
-export * from './lib/scenario-db'
-export * from './lib/scenario.service'
-
 
 export * from './lib/ConceptEnum'
 export * from './lib/LinerStateEnum'
