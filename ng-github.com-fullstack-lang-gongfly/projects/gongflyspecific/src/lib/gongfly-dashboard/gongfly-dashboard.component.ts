66cc268741e0ba359711f0f5b34e36f1b5adfc34
peugeot <thomaspeugeot@yahoo.fr>
2024-05-09 08:53:18 +0200

    Port to ng17 Fixes #17

--- ng-github.com-fullstack-lang-gongfly/projects/gongflyspecific/src/lib/gongfly-dashboard/gongfly-dashboard.component.ts
+++ ng-github.com-fullstack-lang-gongfly/projects/gongflyspecific/src/lib/gongfly-dashboard/gongfly-dashboard.component.ts
@@ -7,8 +7,7 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MatOptionModule } from '@angular/material/core'
 
 import { EngineControlComponent } from '@vendored_components/github.com/fullstack-lang/gongsim/ng-github.com-fullstack-lang-gongsim/projects/gongsimspecific/src/public-api'
-import { MapoptionsComponent } from '@vendored_components/github.com/fullstack-lang/gongsim/ng-github.com-fullstack-lang-gongsim/projects/gongsimspecific/src/lib/engine-control/engine-control.component'
-
+import { MapoptionsComponent } from '@vendored_components/github.com/fullstack-lang/gongleaflet/ng-github.com-fullstack-lang-gongleaflet/projects/gongleafletspecific/src/public-api'
 
 @Component({
   selector: 'lib-gongfly-dashboard',
